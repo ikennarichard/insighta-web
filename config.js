c96041b5be92b21bf438181de7dfdb11b0c0ea89
Ikenna Richard <oguejioforrichard@gmail.com>
2026-04-30 09:53:53 +0100

    update

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 const CONFIG = {
-    // Set window.INSIGHTA_API_URL before loading this file to override
-    API_URL: window.INSIGHTA_API_URL || "http://localhost:8080",
+    API_URL: window.INSIGHTA_API_URL
 };
